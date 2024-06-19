9596a9ced4ade41629ea209cc1c4bfc3d01bbd18
Kris Hagel <hagelk@psd401.net>
2024-06-19 15:38:37 -0700

    Minor changes to start implementing new CSS

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -1,15 +1,15 @@
 import chatgpt from "../assets/chatgpt.svg";
 import youtubelogo from "../assets/youtubelogo.svg";
+import psdlogo from "../assets/psd_logo-2color-fulllandscape.svg";
 
 
 function Logo() {
     return (
         <div>
             <div id="logo">
-                <img id="img-chatgpt" src={chatgpt}/>
-                <img id="img-youtube" src={youtubelogo}/>
+                <img id="img-psdlogo" src={psdlogo}/>
             </div>
-            <div id="logo-text">Chat with YouTube</div>
+            <div id="logo-text">PSD AI</div>
         </div>
     )
 }
